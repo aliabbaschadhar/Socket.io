129a30b0de86cb725e748fa54e09f565f43e4c76
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-08-26 15:56:21 +0500

    basic setup done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,34 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors"
 
-const app = express();
+
 const PORT = process.env.PORT || 3000;
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+});
+
+
+app.use(cors())
+app.use(express.json())
+
+io.on("connection", (socket) => {
+  console.log("User connected")
+  console.log(socket.id)
+})
 
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
 
-app.listen(PORT, () => {
+
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
